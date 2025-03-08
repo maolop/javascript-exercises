5b4421a363822c741b3c86d66e20112d28219f50
maolop <13868738+maolop@users.noreply.github.com>
2025-03-08 12:54:09 +0200

    finish exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    if (repeats < 0)
+        return "ERROR"
 
+    let newString = ""
+    while (repeats > 0) {
+        newString += string
+        repeats--
+    }
+    return newString
 };
 
 // Do not edit below this line
