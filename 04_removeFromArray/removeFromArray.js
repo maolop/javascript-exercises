7475f7fa370fe28ee36003d4c39eccbecc342745
maolop <13868738+maolop@users.noreply.github.com>
2025-03-08 13:24:18 +0200

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = []
+    for (item of array) {
+        if (!toRemove.includes(item))
+            newArray.push(item)
+    }
+
+    return newArray
 };
 
 // Do not edit below this line
