697002d52f9c80bd0b357bd694f218bde1e32bea
maolop <13868738+maolop@users.noreply.github.com>
2025-03-08 19:10:27 +0200

    complete ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,45 @@
-const palindromes = function () {
+function isAlphaChar(char) {
+    let charCode = char.charCodeAt(0)
 
-};
+    // Check if character is A-Z
+    if (charCode >= 65 && charCode <= 90)
+        return true
+
+    // Check if character is a-z
+    if (charCode >= 97 && charCode <= 122)
+        return true
+
+    // Check if character is 0-9
+    if (charCode >= 48 && charCode <= 57)
+        return true
+
+    return false
+}
+
+function cleanString(string) {
+    let alphaString = ""
+
+    for (let char of string)
+        if (isAlphaChar(char)) alphaString += char
+
+    return alphaString
+}
+
+function reverseString(string) {
+    let reversedString = ""
+
+    for (let i = string.length -1; i >= 0; i--)
+        reversedString += string[i]
+
+    return reversedString
+}
+
+const palindromes = function (stringToCheck) {
+    stringToCheck = cleanString(stringToCheck).toLowerCase()
+    let reversedString = reverseString(stringToCheck).toLowerCase()
+
+    return stringToCheck === reversedString
+}
 
 // Do not edit below this line
 module.exports = palindromes;
