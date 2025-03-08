6e08466c1a5492b28c5a7c8465771855a4899e90
maolop <13868738+maolop@users.noreply.github.com>
2025-03-08 19:52:56 +0200

    complete ex11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = []
+    for (item of books)
+        titles.push(item.title)
 
+    return titles
 };
 
 // Do not edit below this line
