87dda373e094c756c0a7e03ae0560446878e0269
maolop <13868738+maolop@users.noreply.github.com>
2025-03-08 19:32:56 +0200

    complete ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let fiboSequence = [1, 1]
+    index = Number(index)
 
+    if (index === 0) return 0
+    if (index < 0) return "OOPS"
+
+    while (fiboSequence.length < index) {
+        fiboSequence.push(fiboSequence[fiboSequence.length - 2] + fiboSequence[fiboSequence.length - 1])
+    }
+
+    return fiboSequence[fiboSequence.length - 1]
 };
 
 // Do not edit below this line
