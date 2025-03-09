f69ad4b757a917416e5257d71739c9197224bbbd
maolop <13868738+maolop@users.noreply.github.com>
2025-03-09 10:55:04 +0200

    complete ex11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let maxAge = 0
+
+    for (person of people) {
+        if (!("yearOfDeath" in person))
+            person.age = new Date().getFullYear() - person.yearOfBirth
+        else
+            person.age = person.yearOfDeath - person.yearOfBirth
+    }
+
+    for (person of people)
+        if (person.age > maxAge) maxAge = person.age
+
+    for (person of people)
+        if (person.age === maxAge) return person
 
 };
 
